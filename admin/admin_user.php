2f8801dee8c00ea151fd25d90d591be5b4365bf1
Shahadat Jahan <shahadat.jahan@gmail.com>
2020-11-25 00:44:01 +0600

    New Commit

--- admin/admin_user.php
+++ admin/admin_user.php
@@ -1,38 +1,44 @@
-<?php 
-	require("haeder.php");	
+<?php
+require("haeder.php");
 ?>
 
 <div class="body">
-		<p><input type="button" onclick="location.href='user.php';" value="Add Member" /></p>
+  <p><input type="button" onclick="location.href='user.php';" value="Add Member" /></p>
 
-	<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-			
-			<?php 
-			 $user_info = user_lists();
-			 echo '<table class="table-hover" border="1" cellpadding="5" cellspacing="0"><thead><tr><th >ID</th><th>Username</th><th>First Name</th><th>Last Name</th><th>Email</th><th>Action</th></tr></thead><tbody>';
-			while ($row = $user_info->fetch_assoc()) {
-				echo '<tr><td>'. $row['id'] .'</td><td>'.$row['username'].'</td><td>'.$row['first_name'].'</td><td>'.$row['last_name'].'</td><td>'.$row['email'].'</td><td><button><a href="user.php?id='.$row['id'].'">Edit</a></button> | <button><a onclick="return ConfirmDelete();" href="user_delete.php?id='.$row['id'].'">Delete</a></button></td></tr>';
-			}
-			echo'</tbody></table>';
-			 ?>
-	
-		</form>
-	</div>
-<script>
-    function ConfirmEdit()
-    {
-      var x = confirm("Are you sure you want to edit?");
-      if (x)
-          return true;
-      else
-        return false;
-    }function ConfirmDelete()
-    {
-      var x = confirm("Are you sure you want to delete?");
-      if (x)
-          return true;
-      else
-        return false;
+  <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+
+    <?php
+    $user_info = user_lists();
+    echo '<table class="table-hover" border="1" cellpadding="5" cellspacing="0"><thead><tr><th >ID</th>
+       <th>Username</th><th>First Name</th><th>Last Name</th><th>Email</th><th>Action</th></tr></thead>
+       <tbody>';
+    while ($row = $user_info->fetch_assoc()) {
+      echo '<tr><td>' . $row['id'] . '</td><td>' . $row['username'] . '</td><td>' . $row['first_name'] . '</td>
+        <td>' . $row['last_name'] . '</td><td>' . $row['email'] . '</td><td><button>
+        <a href="user.php?id=' . $row['id'] . '">Edit</a></button> | <button>
+        <a onclick="return ConfirmDelete();" href="user_delete.php?id=' . $row['id'] . '">Delete</a></button>
+        </td></tr>';
     }
+    echo '</tbody></table>';
+    ?>
+
+  </form>
+</div>
+<script>
+  function ConfirmEdit() {
+    var x = confirm("Are you sure you want to edit?");
+    if (x)
+      return true;
+    else
+      return false;
+  }
+
+  function ConfirmDelete() {
+    var x = confirm("Are you sure you want to delete?");
+    if (x)
+      return true;
+    else
+      return false;
+  }
 </script>
 <?php require("footer.php"); ?>
--- dashboard.php
+++ dashboard.php
@@ -1,64 +1,63 @@
 <?php
 
-	require("haeder.php");
-	
-	$user_id = $task_id = $group_id ="";
+require("haeder.php");
 
-	if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["submit"])) {
-		$user_id = test_input($_POST["username"]);
-		$task_id = test_input($_POST["title"]);
-		$group_id = test_input($_POST["group_name"]);
+$user_id = $task_id = $group_id = "";
 
-		Assign($user_id, $task_id, $group_id);
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["submit"])) {
+	$user_id = test_input($_POST["username"]);
+	$task_id = test_input($_POST["title"]);
+	$group_id = test_input($_POST["group_name"]);
 
-	 }
+	Assign($user_id, $task_id, $group_id);
+}
 
-	function test_input($data){
-			 	$data = trim($data);
-			 	$data = stripcslashes($data);
-			 	$data = htmlspecialchars($data);
-			 	return $data;
-			 }
+function test_input($data)
+{
+	$data = trim($data);
+	$data = stripcslashes($data);
+	$data = htmlspecialchars($data);
+	return $data;
+}
 
 ?>
 
-	<div class="body">
-		
-		<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-			
-			<h2>Dashboard</h2>
+<div class="body">
 
-			<div>
-				<?php
-				$assign_info = AssignLists();
+	<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
 
-				echo '<table border="1" cellpadding="5" cellspacing="0"><thead><tr><th>ID</th><th>Team</th><th>Member</th><th>Task</th></tr></thead><tbody>';
+		<h2>Dashboard</h2>
 
-				while ($row = $assign_info->fetch_assoc()) {
-					$user_name = UserInfo($row["user_id"]);
-					$task_title = TaskInfo($row["task_id"]);
-    				$group_name = GroupInfo($row["group_id"]);
-				echo '<tr><td>'. $row['id'] .'</td><td>'.$group_name .'</td><td>'.$user_name .'</td><td>'.$task_title .'</td></tr>';
-				}
-				echo '</tbody></table>';
+		<div>
+			<?php
+			$assign_info = AssignLists();
+
+			echo '<table border="1" cellpadding="5" cellspacing="0"><thead><tr><th>ID</th><th>Team</th><th>Member</th><th>Task</th></tr></thead><tbody>';
+
+			while ($row = $assign_info->fetch_assoc()) {
+				$user_name = UserInfo($row["user_id"]);
+				$task_title = TaskInfo($row["task_id"]);
+				$group_name = GroupInfo($row["group_id"]);
+				echo '<tr><td>' . $row['id'] . '</td><td>' . $group_name . '</td><td>' . $user_name . '</td><td>' . $task_title . '</td></tr>';
+			}
+			echo '</tbody></table>';
 
 			?>
-			</div>
+		</div>
 
-		</form>
+	</form>
 
-	</div>
+</div>
 
 <script>
-    function ConfirmDelete()
-    {
-      var x = confirm("Are you sure you want to delete?");
-      if (x)
-          return true;
-      else
-        return false;
-    }
-</script>    
+	function ConfirmDelete() {
+		var x = confirm("Are you sure you want to delete?");
+		if (x)
+			return true;
+		else
+			return false;
+	}
+</script>
 
 
 <?php require("footer.php"); ?>
